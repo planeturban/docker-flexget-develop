4661936d3baa75d13621fae1509d0d5d956f1b71
planeturban <ube@alienautopsy.net>
2018-08-15 21:59:37 +0200

    automatic: new version

--- update_version.sh
+++ update_version.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
+git pull || exit
 v=$(cat VERSION)
 new=$(git ls-remote --tags https://github.com/Flexget/Flexget.git | sort -t '/' -k 3 -V | egrep -v "\}" | awk '{print $2}' | cut -d / -f3 | tail -1)
 if [ $v != $new ]
 then
 echo $new > VERSION
-echo update
 fi 
+git commit -am "automatic: new version"
