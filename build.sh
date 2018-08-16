c503ef6f67e7cddadd45d144e8f27990757abe41
planeturban <ube@alienautopsy.net>
2018-08-16 10:12:26 +0200

    automatic: new version

--- build.sh
+++ build.sh
@@ -4,14 +4,6 @@ v=$(cat VERSION)
 new=$(git ls-remote --tags https://github.com/Flexget/Flexget.git | sort -t '/' -k 3 -V | egrep -v "\}" | awk '{print $2}' | cut -d / -f3 | tail -1)
 if [ $v != $new ]
 then
-	echo $new > VERSION
-	echo git tag $new
 	img=planeturban/docker-flexget-develop
-	docker login -u $dhu -p $dhp
-	docker build -t $img:$new . 
-	docker tag $img:$new $img:latest
-	docker push $img
-	docker rmi $img:$new $img:latest
-	git commit -am "automatic: new version"
+	docker login -u $dhu -p $dhp && docker build --no-cache -t $img:$new . && docker tag $img:$new $img:latest && docker push $img && echo $new > VERSION && git tag $new ; git commit -am "automatic: new version"; git push
 fi 
-git push
